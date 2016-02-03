6b3e3022f4b757d86b17fd44af0b6a475a0c89b3
Your Name <you@example.com>
2016-02-03 15:37:12 +0000

    Debut du fichier models.py Alex 03.02.2016

--- upto/models.py
+++ upto/models.py
@@ -1,3 +1,8 @@
-from django.db import models
+from django.db import models as orimodels
+from mongo_auth.contrib.models import User
+from djangotoolbox.fields import ListField, EmbeddedModelField
+import mongoengine
 
-# Create your models here.
+class Users(mongoengine.Document):
+
+	user = EmbeddedModelField('User')
