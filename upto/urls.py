1d6e982db0748ccd9006c7723aaf37245dc7b73b
Your Name <you@example.com>
2016-02-09 20:45:34 +0100

    + Add view user_info and template accountDetails  - Vincent

--- upto/urls.py
+++ upto/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^account/$', views.account, name='account'),
+    url(r'^account/(?P<nom_user>[a-z]+)/$', views.user_info, name='user_info')
 ]
--- upto/views.py
+++ upto/views.py
@@ -2,14 +2,25 @@ from django.shortcuts import render
 from django.http import Http404, HttpResponse
 from .models import Users, Wishes
 
+
 def index(request):
     return render(request, 'upto/index.html')
 
+
 def account(request):
     # test with a user
-    user_id = '56b8f12d43035630c76c9360'
+    user_id = '56b8ec3d4303561aa65b05c1'
     user = Users.objects.get(id=user_id)
     context = {
         'one_user': user,
     }
     return render(request, 'upto/myAccount.html', context)
+
+
+def user_info(request, nom_user):
+    user_name = nom_user
+    user = Users.objects.get(user__username=user_name)
+    context = {
+        'user': user,
+    }
+    return render(request, 'upto/accountDetails.html', context)
