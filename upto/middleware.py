41a27dc18bc40ad49ee3b0e2f01fee496b03c6b3
Your Name <you@example.com>
2016-02-17 19:40:27 +0100

    [18.02.2016] - Marc + Config settings.py  for Rest Framework + Add serializers.py + middleware.py

---
+++ upto/middleware.py
@@ -0,0 +1,6 @@
+def process_response(self, request, response):
+        response['Access-Control-Allow-Origin'] = '*'
+        response['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, PUT'
+        #response['Content-Type'] = 'application/json'
+        #response['Accept'] = 'application/json'
+        return response
--- upto/views.py
+++ upto/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import Http404, HttpResponse
 from .models import Users, Wishes, Events
+from serializers import MySerializer
+from rest_framework.decorators import api_view, renderer_classes, permission_classes
+from rest_framework.renderers import TemplateHTMLRenderer, JSONRenderer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+
 
 def index(request):
     return render(request, 'upto/index.html')
@@ -24,9 +30,40 @@ def user_info(request, nom_user):
 
 def allwishesAndEvent(request):
 
-    w = Users.objects.order_by('events_Owned.start_date')#.only('wishes')
-    w = Users.objects.order_by('events_Owned.start_date')
+    w = Users.objects.order_by('events_Owned.start_date').only('wishes')
+    e = Users.objects.order_by('events_Owned.start_date').only('events_Owned')
     context = {
-        'wishes': w,
+        'user_wishes': w,
+        'user_events': e,
     }
+    if request.accepted_renderer.format == 'html':
+
     return render(request, 'upto/wishes.html', context)
+
+@api_view(('GET',))
+@permission_classes((IsAuthenticated, ))
+@renderer_classes((TemplateHTMLRenderer, JSONRenderer))
+def viewprofile(request, userid):
+        if request.user.is_authenticated():
+                pro = Profile.objects.get(user_id=userid)
+                if request.accepted_renderer.format == 'html':
+                        data = {'profile' : pro}
+                        return Response(data, template_name='network/profile.html')
+                serializer = MySerializer(instance=pro)
+                data = serializer.data
+                return Response(data)
+
+@api_view(('DATA',))
+@renderer_classes((TemplateHTMLRenderer, JSONRenderer))
+@permission_classes((AllowAny, ))
+def list_profiles(request):
+        ch = request.DATA.get('chaine')
+        queryset = Profile.objects.all().filter(user__username__icontains=ch)
+
+        if request.accepted_renderer.format == 'html':
+                data = {'profile' : queryset, 'request' : request}
+                return Response(data, template_name='network/searchresult.html')
+
+        serializer = MySerializer(instance=queryset, many=True)
+        data = serializer.data
+        return Response(data)
