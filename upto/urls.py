9046d8b0f43a0fcac6c4caf673a51075741a460a
Your Name <you@example.com>
2016-02-19 19:11:45 +0100

    [19.02.2016] - Marc +

--- upto/urls.py
+++ upto/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^account/$', views.account, name='account'),
     url(r'^wishes/$', views.allwishesAndEvent, name='wishes'),
-    url(r'^userdetails/(?P<username>\w+)/$', views.userdetails, name='userdetails')
+    url(r'^event/(?P<_event_id>\w+)/$', views.getEventInfo, name='eventDetails'),
+    url(r'^account/(?P<nom_user>[a-z]+)/$', views.user_info, name='user_info')
 ]
