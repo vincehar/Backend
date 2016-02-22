6f36d0f68980d311750a62f79770f9b1a0fc9bab
Your Name <you@example.com>
2016-02-22 18:45:07 +0100

    + Add Class UsersDetailsSerializer and modification of userdetails in views.py - Vincent

--- upto/views.py
+++ upto/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import Http404, HttpResponse
 from .models import Users, Wishes, Events
-#from serializers import MySerializer
+from serializers import UserDetailsSerializer
 from rest_framework.decorators import api_view, renderer_classes, permission_classes
 from rest_framework.renderers import TemplateHTMLRenderer, JSONRenderer
 from rest_framework.permissions import IsAuthenticated, AllowAny
@@ -23,44 +23,45 @@ def account(request):
     }
     return render(request, 'upto/myaccount.html', context)
 
+
 @api_view(('GET',))
-@renderer_classes((TemplateHTMLRenderer, JSONRenderer))
-def user_info(request, nom_user):
-    user_name = nom_user
-    user = Users.objects.get(user__username=user_name)
-    context = {
-        'user': user,
-    }
-    #if request.accepted_renderer.format == 'html':
-    return render(request, 'upto/accountDetails.html', context)
-    #else:
-    #    serializer = MySerializer(instance=context)
-    #    data = serializer.data
-    #    return Response(data)
+@permission_classes((AllowAny, ))
+@renderer_classes((JSONRenderer, TemplateHTMLRenderer))
+def userdetails(request, username):
+
+    user = Users.objects.get(user__username=username)
+
+    if request.accepted_renderer.format == 'html':
+        context = {
+            'user': user,
+        }
+        return render(request, 'upto/userdetails.html', context)
+
+    serializer = UserDetailsSerializer(instance=user)
+    return Response(serializer.data)
+
 
 @renderer_classes((TemplateHTMLRenderer, JSONRenderer))
 def allwishesAndEvent(request):
-    tmplst = list()
-    usersList = Users.objects
-    for user in usersList:
-        for event in user.events_Owned:
-                tmplst.append(event)
-        for wish in user.wishes:
-                tmplst.append(wish)
-    context = {
-        'eventsList': sorted(tmplst, key=methodcaller('get_ref_date'), reverse=True),
-    }
+    eventsList = list()
+    wishesAndEvents = Users.objects
 
+    for users in wishesAndEvents:
+        #1 - lister tous les wishes + users
+        for event in users.events_Owned:
+            eventsList.append(event)
+         #2 - lister tous les events + users
+        for wish in users.wishes:
+            eventsList.append(wish)
+
+    #3 - Append list Object + users (string)
+    context = {
+        'wishesAndEvent': wishesAndEvents,
+        'eventsList': eventsList,#sorted(eventsList, key=methodcaller('get_ref_date'), reverse=True),
+    }
     #if request.accepted_renderer.format == 'html':
     return render(request, 'upto/wishes.html', context)
     #else:
     #    serializer = MySerializer(instance=context)
      #   data = serializer.data
       #  return Response(data)
-
-def getEventInfo(request, _event_id):
-    context = {
-        'currentEvent': Users.objects.get(events_Owned__event_id=_event_id)
-    }
-    return render(request, 'upto/eventDetails.html', context)
-
